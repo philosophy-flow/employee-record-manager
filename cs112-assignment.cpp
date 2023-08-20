49693751b7a8f07d3ed153653ba4c5b39f910860
Jacob Stewart <jacob.g.stewart@gmail.com>
2023-08-20 12:11:28 -0400

    adds loop functionality

--- cs112-assignment.cpp
+++ cs112-assignment.cpp
@@ -1,158 +1,157 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-int main() {
-    /*
-    Creates an Employee base class with a name member + id member.
-    Also includes a method that welcomes the user to the program.
+/*
+Creates an Employee base class with a name member + id member.
+Also includes a number of methods shared across some subclasses.
 
-    Protected members are initialized via the constructor when an instance of
-    the class is created.
-    */
-    class Employee {
-       protected:
-        string name;
-        string password;
+Protected members are initialized via the constructor when an instance of
+the class is created.
+*/
+class Employee {
+   protected:
+    string name;
+    string password;
 
-       public:
-        int id;
+   public:
+    int id;
 
-        void logoutUser() {
-            cout
-                << "Thank you for using the program, and have a wonderful day!";
-        }
+    bool validatePassword(string userInput) { return userInput == password; }
 
-        void standbyMessage() {
-            cout << "Please standby for more options, or press (6) to exit the "
-                    "program."
-                 << endl;
-        }
+    void welcomeUser(string permissionLevel) {
+        cout << "Hello, " << name << " - welcome to the Program." << endl;
+        cout << "Your permission level is: " << permissionLevel << endl;
+    }
 
-        void viewRecord() {
-            cout << "Gathering information for selected employee." << endl;
-            standbyMessage();
-        }
+    void logoutUser() {
+        cout << "Thank you for using the program, and have a wonderful day!"
+             << endl;
+    }
 
-        void searchRecords() {
-            cout << "Gathering all employee records." << endl;
-            standbyMessage();
-        }
+    void standbyMessage() {
+        cout << "Please standby for more options, or press (6) to exit the "
+                "program."
+             << endl;
+    }
 
-        Employee(string userName, int userId, string userPassword) {
-            name = userName;
-            id = userId;
-            password = userPassword;
-        }
-    };
+    void viewRecord() {
+        cout << "Gathering information for selected employee." << endl;
+        standbyMessage();
+    }
 
-    // Creates a HumanResources subclass that extends the Employee class.
-    class HumanResources : public Employee {
-       public:
-        void welcomeUser() {
-            cout << "Hello, " << name << " - welcome to the Program." << endl;
-            cout << "Your permission level is: HUMAN RESOURCES (ADMIN)."
-                 << endl;
-        }
+    void searchRecords() {
+        cout << "Gathering all employee records." << endl;
+        standbyMessage();
+    }
 
-        void displayMenu() {
-            cout << "--------" << endl;
-            cout << "Select an option by pressing the corresponding key on "
-                    "your keyboard:"
-                 << endl;
-            cout << "(1) | View Selected Employee Record" << endl;
-            cout << "(2) | Search Employee Records " << endl;
-            cout << "(3) | Add Employee Record" << endl;
-            cout << "(4) | Modify Selected Employee Record" << endl;
-            cout << "(5) | Delete Selected Employee Record" << endl;
-            cout << "(6) | Logout & Exit Program" << endl;
-            cout << "--------" << endl;
-        }
+    virtual void displayMenu() {}
 
-        void addRecord() {
-            cout << "Booting EmployeeGen Software." << endl;
-            standbyMessage();
-        }
+    Employee(string userName, int userId, string userPassword) {
+        name = userName;
+        id = userId;
+        password = userPassword;
+    }
+};
 
-        void modifyRecord() {
-            cout << "Gathering information for modification." << endl;
-            standbyMessage();
-        }
+// Creates a HumanResources subclass that extends the Employee class.
+class HumanResources : public Employee {
+   public:
+    void displayMenu() {
+        Employee::welcomeUser("HUMAN RESOURCES (ADMIN).");
+        cout << "--------" << endl;
+        cout << "Select an option by pressing the corresponding key on "
+                "your keyboard:"
+             << endl;
+        cout << "(1) | View Selected Employee Record" << endl;
+        cout << "(2) | Search Employee Records " << endl;
+        cout << "(3) | Add Employee Record" << endl;
+        cout << "(4) | Modify Selected Employee Record" << endl;
+        cout << "(5) | Delete Selected Employee Record" << endl;
+        cout << "(6) | Logout & Exit Program" << endl;
+        cout << "--------" << endl;
+    }
 
-        void deleteRecord() {
-            cout << "Gathering information for deletion." << endl;
-            standbyMessage();
-        }
+    void addRecord() {
+        cout << "Booting EmployeeGen Software." << endl;
+        standbyMessage();
+    }
 
-        HumanResources(string userName, int userId, string userPassword)
-            : Employee(userName, userId, userPassword) {}
-    };
+    void modifyRecord() {
+        cout << "Gathering information for modification." << endl;
+        standbyMessage();
+    }
 
-    // Creates a Management subclass that extends the Employee class.
-    class Management : public Employee {
-       public:
-        void welcomeUser() {
-            cout << "Hello, " << name << " - welcome to the Program." << endl;
-            cout << "Your permission level is: MANAGEMENT." << endl;
-        }
+    void deleteRecord() {
+        cout << "Gathering information for deletion." << endl;
+        standbyMessage();
+    }
 
-        void displayMenu() {
-            cout << "--------" << endl;
-            cout << "Select an option by pressing the corresponding key on "
-                    "your keyboard:"
-                 << endl;
-            cout << "(1) | View Selected Employee Record" << endl;
-            cout << "(2) | Search Employee Records " << endl;
-            cout << "(6) | Logout & Exit Program" << endl;
-            cout << "--------" << endl;
-        }
+    HumanResources(string userName, int userId, string userPassword)
+        : Employee(userName, userId, userPassword) {}
+};
 
-        Management(string userName, int userId, string userPassword)
-            : Employee(userName, userId, userPassword) {}
-    };
+// Creates a Management subclass that extends the Employee class.
+class Management : public Employee {
+   public:
+    void displayMenu() {
+        Employee::welcomeUser("MANAGEMENT");
+        cout << "--------" << endl;
+        cout << "Select an option by pressing the corresponding key on "
+                "your keyboard:"
+             << endl;
+        cout << "(1) | View Selected Employee Record" << endl;
+        cout << "(2) | Search Employee Records " << endl;
+        cout << "(6) | Logout & Exit Program" << endl;
+        cout << "--------" << endl;
+    }
 
-    // Creates a GeneralEmployee subclass that extends the Employee class.
-    class GeneralEmployee : public Employee {
-       public:
-        void welcomeUser() {
-            cout << "Hello, " << name << " - welcome to the Program." << endl;
-            cout << "Your permission level is: EMPLOYEE." << endl;
-        }
+    Management(string userName, int userId, string userPassword)
+        : Employee(userName, userId, userPassword) {}
+};
 
-        void displayMenu() {
-            cout << "--------" << endl;
-            cout << "Select an option by pressing the corresponding key on "
-                    "your keyboard:"
-                 << endl;
-            cout << "(1) | View Your Employee Record" << endl;
-            cout << "(6) | Logout & Exit Program" << endl;
-            cout << "--------" << endl;
-        }
+// Creates a GeneralEmployee subclass that extends the Employee class.
+class GeneralEmployee : public Employee {
+   public:
+    void displayMenu() {
+        Employee::welcomeUser("EMPLOYEE");
+        cout << "--------" << endl;
+        cout << "Select an option by pressing the corresponding key on "
+                "your keyboard:"
+             << endl;
+        cout << "(1) | View Your Employee Record" << endl;
+        cout << "(6) | Logout & Exit Program" << endl;
+        cout << "--------" << endl;
+    }
 
-        void viewRecord() {
-            cout << "Gathering your employee record." << endl;
-            standbyMessage();
-        }
+    void viewRecord() {
+        cout << "Gathering your employee record." << endl;
+        standbyMessage();
+    }
 
-        void searchRecords() {
-            cout << "You are not authorized to search records." << endl;
-            standbyMessage();
-        }
+    void searchRecords() {
+        cout << "You are not authorized to search records." << endl;
+        standbyMessage();
+    }
 
-        GeneralEmployee(string userName, int userId, string userPassword)
-            : Employee(userName, userId, userPassword) {}
-    };
+    GeneralEmployee(string userName, int userId, string userPassword)
+        : Employee(userName, userId, userPassword) {}
+};
 
-    /*
-    Creates 3 test IDs / passwords for validation demo purposes.
-    User input will be validated against this static content.
-    */
-    static HumanResources hrDirector("Hannah", 1552, "pass123");
-    static Management midLevelManager("Michael", 2461, "pass456");
-    static GeneralEmployee associate("Gillian", 7717, "pass789");
-    Employee* employeeArray[3] = {&hrDirector, &midLevelManager, &associate};
+/*
+Creates 3 test IDs / passwords for validation demo purposes.
+User input will be validated against this static content.
+*/
+HumanResources hrDirector("Hannah", 1552, "pass123");
+Management midLevelManager("Michael", 2461, "pass456");
+GeneralEmployee associate("Gillian", 7717, "pass789");
+Employee* employeeArray[3] = {&hrDirector, &midLevelManager, &associate};
 
+/*
+ Takes an array of employees and validates credentials against user input
+ If validation is successful, then an active employee is returned.
+*/
+Employee* validateUser(Employee* inputArray[], int employeeCount) {
     // Prints introductory program text
     cout << " " << endl;
     cout << "**************************************" << endl;
@@ -165,7 +164,7 @@ int main() {
     cout << "Enter your Employee ID: ";
     cin >> userId;
 
-    // Clears any errors from first input and prepare cin for second input
+    // Clears any errors from first input and prepares cin for second input
     cin.clear();
     cin.ignore(1000, '\n');
 
@@ -176,21 +175,45 @@ int main() {
     cout << endl;
 
     /*
-    Searches existing employee IDs and attempts to match user input.
-    If ID match, then validates the password. Else, alerts user.
+    Searches existing employee IDs and attempts to validates input.
+    If validation successful proceed. Else alert user and exit program.
     */
 
-    bool userFound = false;
-    for (Employee* employee : employeeArray) {
-        if (employee->id == userId) {
-            userFound = true;
-            cout << "password validation here" << endl;
-            // validate password here
-            break;
+    for (int i = 0; i < employeeCount; i++) {
+        if (inputArray[i]->id == userId &&
+            inputArray[i]->validatePassword(userPassword)) {
+            return inputArray[i];
         }
     }
-    if (!userFound) {
-        cout << "Employee not found. Please enter another ID." << endl;
+    cout << "Employee ID or password is incorrect." << endl;
+    return 0;
+}
+
+int main() {
+    // Validates and sets active employee
+    Employee* activeEmployee = validateUser(
+        employeeArray, sizeof(employeeArray) / sizeof(employeeArray[0]));
+
+    // Enters main program loop - user can select an option or close the app
+    if (activeEmployee) {
+        int selectedOption = 0;
+        while (selectedOption != 6) {
+            activeEmployee->displayMenu();
+            cin >> selectedOption;
+
+            cin.clear();
+            cin.ignore(1000, '\n');
+
+            // switch (selectedOption) {
+            //     case 1:
+            //     case 2:
+            //     case 3:
+            //     case 4:
+            //     case 5:
+            //     default:
+            // }
+        }
+        activeEmployee->logoutUser();
     }
 
     // Program should start, welcome the user, and request login
